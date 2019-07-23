da7849b4ac622f9d82af1c228221d39e82b9b889
Blake Schwartzbach <blake2323@gmail.com>
2019-07-23 00:17:18 -0700

    07231216am

--- app/app.py
+++ app/app.py
@@ -1,17 +1,25 @@
 
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect
+#from flask_pymongo import PyMongo
+from pymongo import MongoClient
 import scrape_mars
+
+#syntax to create a MongoClient in Python
+client = MongoClient("mongodb://127.0.0.1:27017");
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-
+       mars = client.db.mars.find_one()
+       return render_template("index.html", mars=mars)
+       
 @app.route('/scrape')
 def scrape():
-    mars_data = scrape_mars.scrape_all()
-
+       mars = client.db.mars
+       mars_data = scrape_mars.scrape_all()
+       mars.update({}, mars_data, upsert=True)
+       return redirect('/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app/scrape_mars.py
+++ app/scrape_mars.py
@@ -6,12 +6,12 @@ def scrape_all():
     executable_path = {'executable_path': '../chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    news_paragraph, news_paragraph = mars_news(browser)
+    news_title, news_paragraph = mars_news(browser)
 
 
     data = {
-        'news_title' : news_title
-        'news_paragraph' : news_paragraph
+        'news_title' : news_title,
+        'news_paragraph' : news_paragraph,
         'featured_image' : featured_image(browser)
     }
 
@@ -19,17 +19,35 @@ def scrape_all():
 
 
 def mars_news(browser):
-    return news_title, news_paragraph
+    
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
     browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
     html = browser.html
     news_soup = BeautifulSoup(html, 'html.parser')
     slide_elem = news_soup.select_one('ul.item_list li.slide')
+    content_element = slide_elem.find('div', class_="content_title")
+    news_title = content_element.get_text()
+    news_paragraph = slide_elem.find('div', class_="article_teaser_body").text
+    return news_title, news_paragraph
 
 
 
 
-def feature_img(browser):
-    return img.url
+def featured_image(browser):
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+    browser.is_element_present_by_text('more info   ', wait_time=2)
+    more_info_elem = browser.find_link_by_partial_text('more info')
+    more_info_elem.click()
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+    img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+    img_url_rel
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+        
+    return img_url
+
     
